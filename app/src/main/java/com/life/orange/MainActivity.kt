f0943da525ef282b69ee7819cda307bcbf752d83
zhangly <zhangly@ushareit.com>
2021-11-10 14:23:01 +0800

    [Main][Refactor]Refactor dependency library of build.gradle

--- app/src/main/java/com/life/orange/MainActivity.kt
+++ app/src/main/java/com/life/orange/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.life.orange
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
 import com.life.orange.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
